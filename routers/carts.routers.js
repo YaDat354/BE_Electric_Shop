87bec693efea2adbd3e22a578ad09439a6656ba1
MaiThanhBach22520090 <22520090@gm.uit.edu.vn>
2026-05-17 13:20:37 +0700

    router cate and product.

---
+++ routers/carts.routers.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  createCarts,
+  getAllCarts,
+  getDetailCarts,
+  updateCarts,
+  deleteCarts,
+  getAllCartsbyUserId,
+  CheckoutCarts,
+} = require("../controllers/carts.controllers");
+const { paymentVNPAY, checkPaymentVNPAY } = require("../services/vnpay");
+
+const cartsRouter = express.Router();
+
+cartsRouter.post("/", createCarts);
+cartsRouter.get("/:userid", getAllCartsbyUserId);
+// cartsRouter.get("/:id", getDetailCarts);
+cartsRouter.put("/:id", updateCarts);
+cartsRouter.delete("/:id", deleteCarts);
+cartsRouter.post("/checkout", CheckoutCarts);
+
+module.exports = {
+  cartsRouter,
+};
