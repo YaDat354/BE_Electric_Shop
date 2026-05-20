ea429782f77a13640683b13ba959b2483fe461d7
MaiThanhBach22520090 <22520090@gm.uit.edu.vn>
2026-05-20 21:57:31 +0700

    update feature flash sale.

--- models/flashsales.js
+++ models/flashsales.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Flashsales extends Model {
     /**
@@ -9,18 +7,22 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Flashsaledetails }) {
       // define association here
+      this.hasMany(Flashsaledetails, { foreignKey: 'flashsaleid' });
     }
   }
-  Flashsales.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    start: DataTypes.DATE,
-    end: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Flashsales',
-  });
+  Flashsales.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      start: DataTypes.DATE,
+      end: DataTypes.DATE
+    },
+    {
+      sequelize,
+      modelName: 'Flashsales'
+    }
+  );
   return Flashsales;
 };
