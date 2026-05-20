c17a5678dd670f8f102c160ce403dc0757288a1c
MaiThanhBach22520090 <22520090@gm.uit.edu.vn>
2026-05-21 00:12:08 +0700

    fix notifications.

--- routers/notifications.router.js
+++ routers/notifications.router.js
@@ -11,8 +11,8 @@ const notificationsRouter = express.Router();
 
 notificationsRouter.get('/', getAllNotifications);
 notificationsRouter.get('/unread-count', getUnreadCount);
-notificationsRouter.post('/mark-as-read', markAsRead);
 notificationsRouter.post('/mark-all-read', markAllAsRead);
+notificationsRouter.post('/mark-as-read/:id', markAsRead);
 notificationsRouter.post('/send-pro-noti', sendPromotionNoti);
 
 module.exports = { notificationsRouter };
